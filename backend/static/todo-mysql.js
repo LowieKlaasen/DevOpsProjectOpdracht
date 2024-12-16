980d528f180b517604edde15724710677c52f2d2
Lowie Klaasen <lowie.klaasen@gmail.com>
2024-12-16 09:03:50 +0000

    test

--- backend/static/todo-mysql.js
+++ backend/static/todo-mysql.js
@@ -4,10 +4,10 @@ var mysql      = require('mysql2/promise');
 var pool  = mysql.createPool({
     connectionLimit : 10,
     host            : process.env.MYSQL_HOST,
-    // user            : process.env.MYSQL_USER,
-    user            : "lowie",
-    // password        : process.env.MYSQL_PWD,
-    password        : "a-secret-pw",
+    user            : process.env.MYSQL_USER,
+    // user            : "lowie",
+    password        : process.env.MYSQL_PWD,
+    // password        : "a-secret-pw",
     database        : process.env.MYSQL_DB,
   });
  
